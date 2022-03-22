0d9579eb80ec7313dc326889e8701d231fb0f7ad
Amr Farouk <amr.farouk@wealth-dynamix.com>
2022-03-22 16:26:23 +0200

    End Of Section 3

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'The Dating App';
+  users: any;
 
   constructor(private http: HttpClient){}
-  
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
+
+  ngOnInit() {
+    this.getUsers();
   }
+
+  getUsers(){
+    this.http.get('https://localhost:5001/api/users').subscribe(response => {
+      this.users = response;
+    }, error => {
+      console.log(error);
+    })
+  }
+
 }
